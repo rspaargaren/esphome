d0af6884bbf35fb4d8ad6386fbc2d13a9696d053
rspaargaren <github@speijkers.nl>
2021-01-30 19:38:03 +0000

    Compile ready incl library Dsmr

--- esphome/components/dsmr/__init__.py
+++ esphome/components/dsmr/__init__.py
@@ -1,37 +1,22 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart
-from esphome.core import coroutine
-from esphome.const import (
-    CONF_ID,
-    CONF_LAMBDA,
-    CONF_BRIGHTNESS,
-    CONF_TRIGGER_ID,
-)
+from esphome.const import CONF_ID
 
-dsmr_ns = cg.esphome_ns.namespace("dsmr")
-Dsmr = dsmr_ns.class_("Dsmr", cg.PollingComponent, uart.UARTDevice)
-dsmr_ref = Dsmr.operator("ref")
+DEPENDENCIES = ["uart"]
+AUTO_LOAD = ["sensor", "binary_sensor", "text_sensor"]
+CODEOWNERS = ["@rspaargaren"]
+MULTI_CONF = True
 
 CONF_DSMR_ID = "dsmr_id"
 
-CODEOWNERS = ["@rspaargaren"]
+dsmr_ns = cg.esphome_ns.namespace("espdsmr")
+EspDsmr = dsmr_ns.class_("EspDsmr", cg.PollingComponent, uart.UARTDevice)
 
-DEPENDENCIES = ["uart", "network"]
-
-AUTO_LOAD = ["binary_sensor", "sensor", "text_sensor"]
-
-CONFIG_SCHEMA = cv.All(
+CONFIG_SCHEMA = (
     cv.Schema(
         {
-            cv.GenerateID(): cv.declare_id(DSMR),
-            cv.Optional(CONF_ON_FINISHED_PLAYBACK): automation.validate_automation(
-                {
-                    cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(
-                        DFPlayerFinishedPlaybackTrigger
-                    ),
-                }
-            ),
+            cv.GenerateID(): cv.declare_id(EspDsmr),
         }
     )
     .extend(cv.polling_component_schema("30s"))
@@ -43,18 +28,4 @@ def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
     yield uart.register_uart_device(var, config)
-
-    if CONF_BRIGHTNESS in config:
-        cg.add(var.set_brightness(config[CONF_BRIGHTNESS]))
-    if CONF_LAMBDA in config:
-        lambda_ = yield cg.process_lambda(
-            config[CONF_LAMBDA], [(nextion_ref, "it")], return_type=cg.void
-        )
-        cg.add(var.set_writer(lambda_))
-
-    if CONF_TFT_URL in config:
-        cg.add(var.set_tft_url(config[CONF_TFT_URL]))
-
-    yield display.register_display(var, config)
-
-    yield setup_nextion_(var, config)
+    cg.add_library("matthijskooijman/Dsmr", "0.1")
--- esphome/components/dsmr/esp_dsmr.cpp
+++ esphome/components/dsmr/esp_dsmr.cpp
@@ -1,58 +1,56 @@
 #include "esphome/components/dsmr/esp_dsmr.h"
 #include "esphome/core/log.h"
-#include "dsmr_data.h"
+#include "Arduino.h"
 
 namespace esphome {
-namespace dsmr {
+namespace espdsmr {
 
 static const char *TAG = "dsmr";
 
-void Dsmr::setup() {
-  pinMode(D5, OUTPUT);    // Set D5 as output pin
-  digitalWrite(D5, LOW);  // Set low, don't request message from P1 port
+void EspDsmr::setup() {
+  pinMode(5, OUTPUT);    // Set D5 as output pin
+  digitalWrite(5, LOW);  // Set low, don't request message from P1 port
 }
 
-float Dsmr::get_setup_priority() const { return setup_priority::PROCESSOR; }
+float EspDsmr::get_setup_priority() const { return setup_priority::PROCESSOR; }
 
-void Dsmr::update() {
-  digitalWrite(D5, HIGH);  // TO BE ADJUSTED IN CONFIGURATION
+void EspDsmr::update() {
+  digitalWrite(5, HIGH);  // TO BE ADJUSTED IN CONFIGURATION
 }
 
-void Dsmr::loop() {
+void EspDsmr::loop() {
   while (this->available()) {
     if (this->telegramlen_ >= Max_Telegraph_Length) {  // Buffer overflow
       this->header_found_ = false;
       this->footer_found_ = false;
       ESP_LOGE(TAG, "Error: Message larger than buffer!");
     }
-    this->bytes_read++;
+    this->bytes_read_++;
     this->tel_char_ = this->read();
     if (this->tel_char_ == 47) {  // header: forward slash
-      // ESP_LOGD("DmsrCustom","Header found");
       this->header_found_ = true;
       this->telegramlen_ = 0;
       ESP_LOGD(TAG, "Header found in message");
     }
     if (this->header_found_) {
-      this->telegram[this->telegramlen_] = this->tel_char_;
+      this->telegram_[this->telegramlen_] = this->tel_char_;
       this->telegramlen_++;
       if (this->tel_char_ == 33) {  // footer: exclamation mark
-        ESP_LOGD(TAG "Footer found in message");
+        ESP_LOGD(TAG, "Footer found in message");
         this->footer_found_ = true;
       } else {
         if (this->footer_found_ && this->tel_char_ == 10) {  // last \n after footer
           // Parse message
-          this->MyData data;
+          MyData data;
           // ESP_LOGD("DmsrCustom","Trying to parse");
-          ParseResult<void> res =
-              P1Parser::parse(&data, telegram, telegramlen,
-                              false);  // Parse telegram accoring to data definition. Ignore unknown values.
+          ParseResult<void> res = P1Parser::parse(&data, telegram_, telegramlen_, false);
+          // Parse telegram accoring to data definition. Ignore unknown values.
           if (res.err) {
             // Parsing error, show it
             // Serial.println(res.fullError(telegram, telegram + telegramlen));
           } else {
             // publish_sensors(data);
-            return true;  // break out function
+            // return true;  // break out function
           }
         }
       }
@@ -60,7 +58,7 @@ void Dsmr::loop() {
   }
 }
 
-void Dsmr::dump_config() {}
+void EspDsmr::dump_config() {}
 
-}  // namespace dsmr
+}  // namespace espdsmr
 }  // namespace esphome
